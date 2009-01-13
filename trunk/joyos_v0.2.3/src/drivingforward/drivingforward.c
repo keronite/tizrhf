e07920721a535507c0ed200b467df7716d5409c9
DarkOmega1231 <DarkOmega1231@44109602-e03f-11dd-95e1-755cbddbdd66>
2009-01-13 03:22:22 +0000

    encoder test to demonstrate buggy encoder_reset

--- trunk/joyos_v0.2.3/src/drivingforward/drivingforward.c
+++ trunk/joyos_v0.2.3/src/drivingforward/drivingforward.c
@@ -12,6 +12,10 @@
 #define RIGHT_ENCODER 24
 #define LEFT_ENCODER 25
 
+#define kp .5//1.5
+#define ln_kd 1
+#define ki .05
+
 
 // usetup is called during the calibration period. It must return before the
 // period ends.
@@ -21,34 +25,89 @@ int usetup (void) {
 }
 
 int umain (void) {
+	double prev_vel_r = 0;
+	double prev_vel_l = 0;
+	int acc = 0;
+	int target_speed = 0;
+	encoder_reset(RIGHT_ENCODER);
+	encoder_reset(LEFT_ENCODER);
+	
 	while(1)
 	{
-		//printf("\nright: %d, left: %d, diff: %d", encoder_read(RIGHT_ENCODER), encoder_read(LEFT_ENCODER), encoder_read(LEFT_ENCODER) - encoder_read(EIGHT_ENCODER));
-		/*
-		if ((encoder_read(RIGHT_ENCODER) - encoder_read(LEFT_ENCODER) > 100))
+	double new_vel;
+	double delta = 0;
+	int dif = encoder_read(RIGHT_ENCODER) - encoder_read(LEFT_ENCODER);
+	
+	acc += dif;
+	
+	delta = (target_speed - kp*dif - prev_vel_r);
+	new_vel = prev_vel_r + delta*ln_kd - acc*ki;
+	motor_set_vel(RIGHT_MOTOR, (int)new_vel);
+	prev_vel_r = new_vel;
+	
+	//printf("\n%d   ", (int)new_vel);
+	
+	delta = (target_speed + kp*dif - prev_vel_l);
+	new_vel = prev_vel_l + delta*ln_kd + acc*ki;
+	motor_set_vel(LEFT_MOTOR, (int)new_vel);
+	prev_vel_l = new_vel;
+	
+	//printf("%d  ", (int)new_vel);
+	
+	//printf("%d %c", dif, 1);
+	
+	if (target_speed < FORWARD_SPEED) {
+		target_speed += 16;
+	}
+	
+	if (stop_press()) {
+		motor_set_vel(LEFT_MOTOR, 0);
+		motor_set_vel(RIGHT_MOTOR, 0);
+		target_speed = 0;
+		prev_vel_r = 0;
+		prev_vel_l = 0;
+		acc = 0;
+		encoder_reset(RIGHT_ENCODER);
+		encoder_reset(LEFT_ENCODER);
+		printf("\n%d %d %d %d", encoder_read(RIGHT_ENCODER), encoder_read(LEFT_ENCODER), encoder_read(26), encoder_read(27));
+		go_click();
+	}
+	
+	pause(100);
+	/*
+		if(prev_vel_r <=120)
 		{
-			motor_set_vel(RIGHT_MOTOR, TURNING_SPEED);
-			motor_set_vel(LEFT_MOTOR, FORWARD_SPEED);
-			printf("\nSlight left, diff = %d", encoder_read(RIGHT_ENCODER) - encoder_read(LEFT_ENCODER));
-		}
-		if ((encoder_read(RIGHT_ENCODER) - encoder_read(LEFT_ENCODER) < -100))
-		{
-			motor_set_vel(RIGHT_MOTOR, FORWARD_SPEED);
-			motor_set_vel(LEFT_MOTOR, TURNING_SPEED);
-			printf("\nSlight right, diff = %d", encoder_read(RIGHT_ENCODER) - encoder_read(LEFT_ENCODER));
+			new_vel = prev_vel_r + (FORWARD_SPEED - prev_vel_r)*0.2 + dif;
+			motor_set_vel(RIGHT_MOTOR, new_vel);
+			prev_vel_r = new_vel - dif;
+			printf("\nDiff is %d %c", dif, 1);
+			pause(50);
 		}
 		else
 		{
-			motor_set_vel(RIGHT_MOTOR, FORWARD_SPEED);
-			motor_set_vel(LEFT_MOTOR, FORWARD_SPEED);
-			printf("\nDrive forward, diff = %d", encoder_read(RIGHT_ENCODER) - encoder_read(LEFT_ENCODER));
+			new_vel = prev_vel_r + dif;
+			motor_set_vel(RIGHT_MOTOR, new_vel);
+			prev_vel_r = new_vel - dif;
+			printf("\nDiff is %d %c", dif, 1);
+			pause(50);
 		}
-		*/
-		int dif = encoder_read(RIGHT_ENCODER) - encoder_read(LEFT_ENCODER);
-		motor_set_vel(RIGHT_MOTOR, FORWARD_SPEED + dif);
-		motor_set_vel(LEFT_MOTOR, FORWARD_SPEED - dif);
-		printf("\nDiff is %d %c", dif, 1);
-		pause(50);
+		if(prev_vel_l <=120)
+		{
+			
+			new_vel = prev_vel_l + (FORWARD_SPEED - prev_vel_l)*0.2 - dif;
+			motor_set_vel(LEFT_MOTOR, new_vel);
+			prev_vel_l = new_vel + dif;
+			pause(50);
+		}
+		else
+		{
+			new_vel = prev_vel_l - dif;
+			motor_set_vel(LEFT_MOTOR, new_vel);
+			prev_vel_l = new_vel + dif;
+			pause(50);
+		}
+	*/
+		
 	}
 	return 0;
 }
