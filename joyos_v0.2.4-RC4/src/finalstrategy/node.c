79ab27c3bd115ca6c7be95510c0d0a8f5d15d6f0
DarkOmega1231 <DarkOmega1231@44109602-e03f-11dd-95e1-755cbddbdd66>
2009-01-20 06:20:17 +0000

    Tree stuff should work, test more tomorrow

--- joyos_v0.2.4-RC4/src/finalstrategy/node.c
+++ joyos_v0.2.4-RC4/src/finalstrategy/node.c
@@ -1,20 +1,60 @@
 #include <../src/finalstrategy/node.h>
 #include <../src/finalstrategy/actions.h>
 
+Status test_attempt(Node * node);
+
 Status attempt(Node* node) {
 	return node->_attempt(node);
 }
 
-/*Status travel_to(Node* node) {
-	printf("\nHEY IT WORKED");
-	return SUCCESS;
-}*/
 
 Node travel_node(Position destination, uint8_t use_theta) {
 	Node node;
 	node._attempt = travel_to;
 	node.position = destination;
 	node.use_theta = use_theta;
-//	node.children = Node* [NODE_CHILDREN];
+	for (uint8_t i = 0; i < NODE_CHILDREN; i++) {
+		node.children[i] = NULL;
+	}
 	return node;
+}
+
+Node root_node() {
+	Node node;
+	for (uint8_t i = 0; i < NODE_CHILDREN; i++) {
+		node.children[i] = NULL;
+	}
+	return node;
+}
+
+Node test_node(uint8_t id) {
+	Node node;
+	node._attempt = test_attempt;
+	node.use_theta = id;
+	for (uint8_t i = 0; i < NODE_CHILDREN; i++) {
+		node.children[i] = NULL;
+	}
+	return node;
+}
+
+void add_child(Node* parent, Node* child) {
+	for (uint8_t i = 0; i < NODE_CHILDREN; i++) {
+		if (parent->children[i] == NULL) {
+			parent->children[i] = child;
+			return;
+		}
+	}
+}
+
+Status test_attempt(Node * node) {
+	printf("TEST ATTEMPT");
+	//printf("\n%d", node->use_theta);
+	while(1) {
+		if (go_press()) {
+			return SUCCESS;
+		}
+		else if (stop_press()) {
+			return FAILURE;
+		}
+	}
 }
--- joyos_v0.2.4-RC4/src/finalstrategy/umain.c
+++ joyos_v0.2.4-RC4/src/finalstrategy/umain.c
@@ -8,26 +8,89 @@ uint8_t team_number[2] = {3,0};
 #define LSB_US_PER_DEG 1357.348162*3838.0/3600.0*1028.0/1080.0*1000.0
 #define GYRO_CALIB_TIME 5000
 
+Node construct_strategy();
+
 int usetup () {
 	set_auto_halt(0);
+	return 0;
 	printf_P (PSTR("\nPress go."));
 	go_click ();
 	printf_P (PSTR("\nStabilizing"));
 	pause (500);
 	printf_P (PSTR("\nCalibrating offset."));
 	gyro_init (GYRO_PORT, LSB_US_PER_DEG, GYRO_CALIB_TIME);
+	
+	global_position.x = 0;
+	global_position.y = 0;
 	return 0;
 }
 
 int umain () {
-	global_position.x = 0;
-	global_position.y = 0;
-	Position p;
-	p.x = 1;
-	p.y = 1;
-	p.theta = 0;
-	Node node0 = travel_node(p, false);
-	attempt(&node0);
+	printf("\nGo plz");
+	go_click();
+	//Node current_node = construct_strategy();
+
+	
+	Node root = root_node();
+	Node test0 = test_node(17);
+	add_child(&root,&test0);
+	Node * current_node = &root;
+
+	printf("\nGo again plz");
+	go_click();
+	
+	while(1) {
+		for (uint8_t i = 0; i < NODE_CHILDREN; i++) {
+			printf("\n%d",i);
+			go_click();
+			if (current_node->children[i] != NULL) {
+				printf("\n%d",current_node->children[i]);
+				go_click();
+				printf("\n%d",current_node->children[i]->use_theta);
+				go_click();
+				Status s = attempt(current_node->children[i]);
+				if (s == SUCCESS) {
+					current_node = current_node->children[i];
+					break;
+				}
+			}
+			//if last number here, thrash
+		}
+	}
 	while(1);
 	return 0;
+}
+
+Node construct_strategy() {
+
+	Node root = root_node();
+
+	/*Position p0;
+	p0.x = 1;
+	p0.y = 1;
+	p0.theta = 0;
+	Node node0 = travel_node(p0, false);
+	
+	Position p1;
+	p1.x = 0;
+	p1.y = 0;
+	p1.theta = 0;
+	Node node1 = travel_node(p1, false);
+	
+	add_child(&root,&node0);
+	add_child(&node0,&node1);*/
+	
+//	Node test0 = test_node(17);
+/*	Node test1 = test_node(1);
+	Node test2 = test_node(2);
+	Node test3 = test_node(3);
+	Node test4 = test_node(4);*/
+	
+//	add_child(&root,test0);
+/*	add_child(&test0,&test1);
+	add_child(&test1,&test2);
+	add_child(&test1,&test3);
+	add_child(&test3,&test4);*/
+	
+	return root;
 }
