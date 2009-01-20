138260fbe08cf494556bb21ff0622703000c6ff6
DarkOmega1231 <DarkOmega1231@44109602-e03f-11dd-95e1-755cbddbdd66>
2009-01-20 22:58:55 +0000

    util methods for kevbot

--- joyos_v0.2.4-RC4/src/finalstrategy/util.h
+++ joyos_v0.2.4-RC4/src/finalstrategy/util.h
@@ -4,5 +4,7 @@
 #include <../src/finalstrategy/globals.h>
 
 Position get_ball_position(Ball ball);
+float clamp (float val, float min, float max);
+int degrees_to_servo_units(int degrees);
 
 #endif //_UTIL_H_
