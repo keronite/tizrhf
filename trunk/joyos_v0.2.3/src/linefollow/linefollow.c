07f3aaa93dcc1ca29580abdc785eaefe47b21143
DarkOmega1231 <DarkOmega1231@44109602-e03f-11dd-95e1-755cbddbdd66>
2009-01-21 00:42:04 +0000

    Compass code.  In line follow, sorry!

--- trunk/joyos_v0.2.3/src/linefollow/linefollow.c
+++ trunk/joyos_v0.2.3/src/linefollow/linefollow.c
@@ -1,4 +1,7 @@
 #include <joyos.h>
+#include <happylib.h>
+
+uint8_t team_number[2] = {3,0};
 
 #define FORWARD_SPEED 64
 #define BACKWARD_SPEED 128
@@ -8,64 +11,28 @@
 #define RIGHT_MOTOR 0
 #define LEFT_MOTOR 1
 
-//Shaft encoder convention, 24 is right, 25 is left
-#define RIGHT_ENCODER 24
-#define LEFT_ENCODER 25
+#define FRONT_SERVO 0
 
-
-#define ENCODER_TO_WHEEL_RATIO 15
-#define WHEEL_CIRCUMFERENCE 25.76
-#define WHEEL_TRACK 21.5
-
-#define SQUARE_LENGTH_CM 91.44
-
-#include <lib/geartrain.h>
-
-
-enum state_enum {SETUP, STOP, LINE_FOLLOW} state;
+enum state_enum {SETUP, STOP, LOOK_Y} state;
 
 void setup_state();
 void stop_state();
-void line_follow_state();
+void look_y_state();
 
 void setup_filter();
 void stop_filter();
-void line_follow_filter();
+void look_y_filter();
 
 void soft_stop_motors(int duration);
 
-bool filter_led(uint8_t led_port);
-
-void calibrate_leds();
-
-int sing();
+int degrees_to_servo_units(int degrees);
 
 float clamp (float val, float min, float max);
 
+uint16_t x, y;
 
-uint16_t left_encoder_base, right_encoder_base;
-
-uint8_t led_reading;
-
-#define NUM_LEDS 3
-#define NUM_LED_SAMPLES 5
-
-#define RIGHT_LED 23
-#define MIDDLE_LED 22
-#define LEFT_LED 21
-
-#define RIGHT_LED_INDEX 0
-#define MIDDLE_LED_INDEX 1
-#define LEFT_LED_INDEX 2
-
-#define LED_RESERVED_INDICES 2
-#define LED_OFFSET_INDEX 0
-#define LED_CALIBRATION_INDEX 1
-
-#define LINE_OFFSET_WEAK 5
-#define LINE_OFFSET_STRONG 10
-
-uint16_t led_filter_matrix[NUM_LEDS][NUM_LED_SAMPLES + LED_RESERVED_INDICES];
+#define SHARP_M		13569
+#define SHARP_C		4
 
 // usetup is called during the calibration period. It must return before the
 // period ends.
@@ -84,8 +51,8 @@ int umain (void) {
 			case (SETUP):
 				setup_state();
 				break;
-			case (LINE_FOLLOW):
-				line_follow_state();
+			case (LOOK_Y):
+				look_y_state();
 				break;
 				
 			case (STOP):
@@ -100,55 +67,20 @@ int umain (void) {
 	return 0;
 }
 
-bool filter_led(uint8_t led_port) {
-	uint8_t index;
-	switch (led_port)
-	{
-		case (RIGHT_LED):
-			index = RIGHT_LED_INDEX;
-			break;
-	
-	    case (MIDDLE_LED):
-			index = MIDDLE_LED_INDEX;
-			break;
-	
-	    case (LEFT_LED):
-			index = LEFT_LED_INDEX;
-			break;
-	
-	    default:
-			index = MIDDLE_LED_INDEX;
-			break;
-	}
-	
-	
-	uint8_t offset = (uint8_t)(led_filter_matrix[index][LED_OFFSET_INDEX]);
-	if (offset < 2) {offset = 2;}
-	uint16_t calibration = led_filter_matrix[index][LED_CALIBRATION_INDEX];
-	uint16_t sample = analog_read(led_port);
-	uint16_t current_reading = (sample > calibration); //1 if black, 0 if white
-	led_filter_matrix[index][offset] = current_reading; //Put the new sample in the array
-	offset = (offset + 1 - LED_RESERVED_INDICES)%(NUM_LED_SAMPLES) + LED_RESERVED_INDICES; //Increment offset and keep it in range
-	led_filter_matrix[index][LED_OFFSET_INDEX] = offset; //Store the new offset
-	
-	uint8_t sum = 0; //Count true samples
-	for(uint8_t i = LED_RESERVED_INDICES; i < LED_RESERVED_INDICES + NUM_LED_SAMPLES; i++) {
-		if (led_filter_matrix[index][i] != 0) {
-			sum++;
-		}
-	}
-	
-	if (sum > NUM_LED_SAMPLES/2) {
-		return true;
-	} else {
-		return false;
-	}
-	
-}
-
 void setup_state() {
 	while (state == SETUP) {
-		calibrate_leds();
+		x = 0;
+		y = 0;
+		irdist_set_calibration (SHARP_M, SHARP_C);
+		
+		printf("\nPress go");
+		go_click();
+		printf("\nStabilizing");
+		pause(1000);
+		
+		printf("\nInitializing");
+		gyro_init(8,1357.348162*3838.0/3600.0*1028.0/1080.0*1000.0,5000);
+		
 		setup_filter();
 	}
 }
@@ -156,88 +88,45 @@ void setup_state() {
 void stop_state() {
 	soft_stop_motors(50);
 	while(state == STOP) {
+		printf("\nx=%d, y=%d", x, y);
 		pause(50);
 		stop_filter();
 	}
 }
 
-void line_follow_state() {
-	led_reading = 0;
-	while(state == LINE_FOLLOW) {
-		//MSB->LSB, left, middle, right
-		  switch (led_reading)
-		  {		
-		    case (1): //001
-		      motor_set_vel(LEFT_MOTOR, FORWARD_SPEED + LINE_OFFSET_STRONG);
-		      motor_set_vel(RIGHT_MOTOR, FORWARD_SPEED - LINE_OFFSET_STRONG);
-			  printf("\nHard Right");
-		      break;
+void look_y_state() {
+	while(state == LOOK_Y) {
+		pause(50);
 		
-		    case (2): //010
-		      motor_set_vel(LEFT_MOTOR, FORWARD_SPEED);
-		      motor_set_vel(RIGHT_MOTOR, FORWARD_SPEED);
-			  printf("\nStraight");
-		      break;
+		int angle = (int)gyro_get_degrees();
 		
-			case (3): //011
-		      motor_set_vel(LEFT_MOTOR, FORWARD_SPEED + LINE_OFFSET_WEAK);
-		      motor_set_vel(RIGHT_MOTOR, FORWARD_SPEED - LINE_OFFSET_WEAK);
-			  printf("\nSoft Right");
-		      break;
+		printf("\n%d  %d", angle, angle%360);
 		
-		    case (4): //100
-		      motor_set_vel(LEFT_MOTOR, FORWARD_SPEED - LINE_OFFSET_STRONG);
-		      motor_set_vel(RIGHT_MOTOR, FORWARD_SPEED + LINE_OFFSET_STRONG);
-			  printf("\nHard Left");
-		      break;
+		servo_set_pos(FRONT_SERVO, degrees_to_servo_units(-angle));
+		pause(1000);
+		x = irdist_read(23);
 		
-		    case (6): //110
-		      motor_set_vel(LEFT_MOTOR, FORWARD_SPEED - LINE_OFFSET_WEAK);
-		      motor_set_vel(RIGHT_MOTOR, FORWARD_SPEED + LINE_OFFSET_WEAK);
-			  printf("\nSoft Left");
-		      break;
-		  }
-		  
-		pause(10);
+		servo_set_pos(FRONT_SERVO, degrees_to_servo_units(-angle - 90));
+		pause(1000);
+		y = irdist_read(23);
 		
-		line_follow_filter();
+		look_y_filter();
 	}
 }
 
 void setup_filter() {
-	if (stop_press()) {
-		state = LINE_FOLLOW;
-	}
+	state = LOOK_Y;
 }
 
 
 void stop_filter() {
 	if (go_press()) {
-		state = LINE_FOLLOW;
+		state = LOOK_Y;
 	}
 }
 
-void line_follow_filter() {
-	bool left = filter_led(LEFT_LED);
-	bool middle = filter_led(MIDDLE_LED);
-	bool right = filter_led(RIGHT_LED);
-	led_reading = (left << 2) + (middle << 1) + (right << 0);
-	/*if (left) {
-		printf("\n1");
-	} else {
-		printf("\n0");
-	}
-	if (middle) {
-		printf("1");
-	} else {
-		printf("0");
-	}
-	if (right) {
-		printf("1");
-	} else {
-		printf("0");
-	}*/
-	//Add stop conditions if gyro and shaft suggest might be done
+void look_y_filter() {
+	state = STOP;
 }
 
 void soft_stop_motors(int duration) {
@@ -255,67 +144,10 @@ float clamp (float val, float min, float max) {
 		return val;
 }
 
-void calibrate_leds() {
-	
-	for (int i = 0; i < NUM_LEDS; i++) {
-		for (int j = 0; j < LED_RESERVED_INDICES + NUM_LED_SAMPLES; j++) {
-			led_filter_matrix[i][j] = 0;
-		}
+int degrees_to_servo_units(int degrees) {
+	int servo_angle = (int)((degrees*256.0/90.0) + 255)%512;
+	if (servo_angle < 0) {
+		servo_angle += 512;
 	}
-	
-	uint16_t samples;
-	
-	printf("\nPlace LEDs on light surface, then press go");
-	
-	go_click();
-	
-	float avg_low_read_l = 0;
-	float avg_low_read_m = 0;
-	float avg_low_read_r = 0;
-	
-	samples = 0;
-	
-	while(!stop_press()) {
-		uint16_t sample_l = analog_read(LEFT_LED);
-		uint16_t sample_m = analog_read(MIDDLE_LED);
-		uint16_t sample_r = analog_read(RIGHT_LED);
-		
-		samples++;
-		
-		avg_low_read_l = (avg_low_read_l*(samples - 1) + sample_l)/(float)samples;
-		avg_low_read_m = (avg_low_read_m*(samples - 1) + sample_m)/(float)samples;
-		avg_low_read_r = (avg_low_read_r*(samples - 1) + sample_r)/(float)samples;
-		
-		printf("\nL:%d  M:%d  R:%d", (int)avg_low_read_l, (int)avg_low_read_m, (int)avg_low_read_r);
-		pause(50);
-	}
-	
-	printf("\nPlace LEDs on dark surface, then press go");
-	
-	go_click();
-	
-	float avg_high_read_l = 0;
-	float avg_high_read_m = 0;
-	float avg_high_read_r = 0;
-	
-	samples = 0;
-	
-	while(!stop_press()) {
-		uint16_t sample_l = analog_read(LEFT_LED);
-		uint16_t sample_m = analog_read(MIDDLE_LED);
-		uint16_t sample_r = analog_read(RIGHT_LED);
-		
-		samples++;
-		
-		avg_high_read_l = (avg_high_read_l*(samples - 1) + sample_l)/(float)samples;
-		avg_high_read_m = (avg_high_read_m*(samples - 1) + sample_m)/(float)samples;
-		avg_high_read_r = (avg_high_read_r*(samples - 1) + sample_r)/(float)samples;
-		
-		printf("\nL:%d  M:%d  R:%d", (int)avg_high_read_l, (int)avg_high_read_m, (int)avg_high_read_r);
-		pause(50);
-	}
-	
-	led_filter_matrix[LEFT_LED_INDEX][LED_CALIBRATION_INDEX] = (avg_high_read_l + avg_low_read_l)/2;
-	led_filter_matrix[MIDDLE_LED_INDEX][LED_CALIBRATION_INDEX] = (avg_high_read_m + avg_low_read_m)/2;
-	led_filter_matrix[RIGHT_LED_INDEX][LED_CALIBRATION_INDEX] = (avg_high_read_r + avg_low_read_r)/2;
+	return servo_angle;
 }
