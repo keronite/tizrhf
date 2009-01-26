083c375ad40a1377cbe662a30f4d9f47980b6d0f
DarkOmega1231 <DarkOmega1231@44109602-e03f-11dd-95e1-755cbddbdd66>
2009-01-26 01:46:55 +0000

    Phail conditions

--- trunk/joyos_v0.2.4-RC4/src/finalstrategy/node.h
+++ trunk/joyos_v0.2.4-RC4/src/finalstrategy/node.h
@@ -28,7 +28,6 @@ Node * find_line_node(Line line);
 Node * corner_orient_node();
 Node * posn_node_front(float angle, uint8_t use_theta);
 Node * posn_node_back(float angle, uint8_t use_theta);
-Node * flag_node();
 void add_child(Node* parent, Node* child);
 
 Status attempt(Node* node);
